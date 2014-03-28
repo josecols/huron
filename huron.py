92aeee4874d24c9f79afe52a775e7d51d517c4c0
josecols <josecolsg@gmail.com>
2014-03-28 02:36:12 -0430

    Mejorado el etiquetamiento de las canciones

--- huron.py
+++ huron.py
@@ -7,45 +7,95 @@ import sys
 import string
 import time
 import re
+import subprocess
 import mutagen.id3
 from mutagen import File
-from mutagen.mp3 import MP3
 
-API_KEY = 'ba584160f1b8564f82d78529eada82179edc0fbb'
-API_VERSION = 3
 
 class Discogs:
     
-    def __init__(self):
+    urls = {'search': 'http://api.discogs.com/database/search?q=%s&artist=%s',
+            'release':'http://api.discogs.com/releases/%s'}
+    
+    def __init__(self, cancion):
         self.key = 'weYwLTpHLKfUBfWuYfdo'
         self.secret = 'itBsbZgbDZyiAIVWbhVaDYzFLfWKmINd'
+        self.cancion = cancion
         
+    def buscar(self):
+        r = requests.get(Discogs.urls['search'] % (self.cancion, self.cancion.artista)).json()
+        return r
+    
+    def etiquetas(self):
+        r = self.buscar()
+        if len(r['results']) > 0:
+            return (r['results'][0]['title'].split('-')[1].strip(),
+                    r['results'][0]['year'].strip(),
+                    ', '.join(g.strip() for g in r['results'][0]['genre']),
+                    self.cover(r['results'][0]['id'])) 
+        return (None, None, None, None)
+    
+    def cover(self, release):
+        r = requests.get(Discogs.urls['release'] % release).json()
+        try:
+            return r['images'][0]['uri'].replace('api.discogs.com', 's.pixogs.com').strip()
+        except KeyError:
+            return None
+         
          
 class Soundcloud:
+    
+    urls = {'track': 'http://api.soundcloud.com/tracks/%s.json?client_id=%s'}
     
     def __init__(self, cliente, cancion):
         self.id = cliente
         self.cancion = cancion
-        
+                    
     @staticmethod
     def atributos(url):
         return (url.partition('client_id=')[2], re.search(r'\d+', url).group())
+   
+    
+class EightTracks:
+
+    headers = {'X-Api-Key': 'ba584160f1b8564f82d78529eada82179edc0fbb', 'X-Api-Version': 3}
+    urls = {'token':'http://8tracks.com/sets/new.json',
+            'next':'http://8tracks.com/sets/%s/next.json?mix_id=%s',
+            'report':'http://8tracks.com/sets/%s/report.json?track_id=[%s]&mix_id=[%s]'}
+    token = requests.get(urls['token'], headers=headers).json()['play_token']
+
+    def __init__(self, mix_url):                
+        self.mix = Mix(*Mix.atributos(requests.get(mix_url + '.json', headers=EightTracks.headers).json()))
+
+    def guardar(self):
+        self.mix.guardar()
+        print "\n Listo."
+        
+    @staticmethod
+    def siguiente(mix_id):
+        return requests.get(EightTracks.urls['next'] % (EightTracks.token, mix_id), headers=EightTracks.headers).json()
+    
+    @staticmethod
+    def reportar(cancion_id, mix_id):
+        requests.get(EightTracks.urls['report'] % (EightTracks.token, cancion_id, mix_id), headers=EightTracks.headers)    
 
 
 class Cancion:
     
     v = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
-    def __init__(self, cancion, nombre, album, fecha, artista, url, archivo):
+    def __init__(self, cancion, nombre, album, fecha, artista, url, archivo, mix_cover):
         self.id = str(cancion)
         self.nombre = ''.join(c for c in nombre if c in Cancion.v)
-        self.album = album
+        self.album = None if album.isdigit() or 'Uploaded by' in album else album
         self.fecha = fecha
         self.artista = artista
+        self.genero = None
         self.url = url
         self.archivo = archivo
-        self.cover = {'url': None, 'archivo': archivo}
+        self.cover = {'url': None, 'archivo': archivo, 'mix': mix_cover}
         self.sc = Soundcloud(*Soundcloud.atributos(url)) if 'soundcloud' in url else None
+        self.discogs = None
         
     def __del__(self):
         try:
@@ -59,7 +109,21 @@ class Cancion:
     def __str__(self):
         return self.nombre + ' - ' + self.artista 
     
-    def etiquetas(self):
+    def actualizar(self):
+        self.discogs = Discogs(self)
+        (album, fecha, genero, cover) = self.discogs.etiquetas()
+        
+        if self.album is None:
+            self.album = self.artista if album is None else album
+        if self.fecha is None:
+            self.fecha = '' if fecha is None else fecha
+        if self.genero is None:
+            self.genero = '' if genero is None else genero        
+        if self.cover['url'] is None:
+            self.cover['url'] = self.cover['mix'] if cover is None else cover
+            self.guardar_cover()
+    
+    def etiquetas(self):                    
         try:
             etiquetas = mutagen.id3.ID3(self.archivo)
         except mutagen.id3.ID3NoHeaderError:
@@ -80,36 +144,29 @@ class Cancion:
             try:
                 etiquetas['TDRC']
             except KeyError:
-                etiquetas['TDRC'] = mutagen.id3.TDRC(encoding=3, text=str(self.fecha))        
-            etiquetas.save(self.archivo)
-            self.albumart()
-            
-    def albumart(self):
-        if self.cover['archivo'].endswith('jpg'):                
-            audio = File(self.archivo)        
+                etiquetas['TDRC'] = mutagen.id3.TDRC(encoding=3, text=str(self.fecha))
             try:
-                audio.tags['APIC:']
-            except KeyError:                   
-                data = open(self.cover['archivo'], 'rb').read()            
-                audio.tags.add(mutagen.id3.APIC(encoding=3, mime='image/jpeg', type=3, desc=u'Cover', data=data))
-                audio.save()
+                etiquetas['TCON']
+            except KeyError:
+                etiquetas['TCON'] = mutagen.id3.TCON(encoding=3, text=self.genero)                
+            etiquetas.save(self.archivo)
             
-    def guardar_cover(self):
-        self.cover['archivo'] = os.path.join(self.cover['archivo'], self.id + '.jpg')
-        with open(self.cover['archivo'], 'wb') as handle:
-            request = requests.get(self.cover['url'], stream=True)
-            for block in request.iter_content():
-                if not block:
-                    break
-                handle.write(block)
-            handle.close()
-    
+            if self.cover['archivo'].endswith('jpg'):                
+                audio = File(self.archivo)        
+                try:
+                    audio.tags['APIC:']
+                except KeyError:                   
+                    data = open(self.cover['archivo'], 'rb').read()            
+                    audio.tags.add(mutagen.id3.APIC(encoding=3, mime='image/jpeg', type=3, desc=u'Cover', data=data))
+                    audio.save()
+                    
     def guardar(self):
         if self.sc:
-            soundcloud = requests.get('http://api.soundcloud.com/tracks/%s.json?client_id=%s' % (self.sc.cancion, self.sc.id)).json()
+            soundcloud = requests.get(Soundcloud.urls['track'] % (self.sc.cancion, self.sc.id)).json()
             if soundcloud['downloadable']:
                 r = requests.get(self.url.replace('stream', 'download'), stream=True)
                 self.archivo += re.search(r'\"(.+?)\"', r.headers.get('content-disposition')).group(0)[1:-1]
+                self.cover['url'] = ''
             else:
                 r = requests.get(self.url, stream=True)
                 self.archivo += self.nombre + '.' + soundcloud['original_format']
@@ -118,20 +175,43 @@ class Cancion:
                     self.guardar_cover()
         else:
             r = requests.get(self.url, stream=True)
-            self.archivo += self.nombre + '.m4a'
-        
+            self.archivo += self.nombre + '.m4a'            
+                
         with open(self.archivo, 'wb') as handle:
             for block in r.iter_content(1024):
                 if not block:
                     break
                 handle.write(block)
-            handle.close()   
-        self.etiquetas()    
+            handle.close()
+            
+        if self.archivo.endswith('m4a'):
+            subprocess.call(['ffmpeg', '-i', os.path.abspath(self.archivo), os.path.abspath(self.archivo[:-3] + 'mp3')],
+                            stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb'))            
+            os.remove(self.archivo)
+            self.archivo = self.archivo[:-3] + 'mp3'
+        self.actualizar()
+        self.etiquetas()           
+            
+    def guardar_cover(self):
+        headers = {'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; WindowsNT)',
+                   'Accept' :'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+                   'Accept-Language' : 'fr-fr,en-us;q=0.7,en;q=0.3',
+                   'Accept-Charset' : 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'
+        }
+        r = requests.get(self.cover['url'], headers=headers, stream=True)
+        self.cover['archivo'] = os.path.join(self.cover['archivo'], self.id + '.jpg')
+        with open(self.cover['archivo'], 'wb') as handle:            
+            for block in r.iter_content():
+                if not block:
+                    break
+                handle.write(block)
+            handle.close() 
         
     @staticmethod
-    def atributos(r, archivo):
+    def atributos(r, archivo, mix_cover):
         return (r['set']['track']['id'], r['set']['track']['name'], r['set']['track']['release_name'],
-                r['set']['track']['year'], r['set']['track']['performer'], r['set']['track']['track_file_stream_url'], archivo)
+                r['set']['track']['year'], r['set']['track']['performer'], r['set']['track']['track_file_stream_url'],
+                archivo, mix_cover)
 
 
 class Mix:
@@ -140,31 +220,14 @@ class Mix:
         self.nombre = ''.join(c for c in nombre if c in Cancion.v)
         self.id = mix_id
         self.canciones = canciones
-        self.cover = cover
-        self.cover_archivo = None
+        self.cover = {'url': cover, 'archivo': None}
         self.directorio = directorio        
-
-    def __del__(self):
-        try:
-            os.remove(self.cover_archivo)
-        except OSError:
-            pass
 
     def __unicode__(self):
         return unicode(self.nombre)
 
     def __str__(self):
         return self.nombre    
-
-    def guardar_cover(self, directorio):
-        self.cover_archivo = directorio + 'cover.jpg'
-        with open(self.cover_archivo, 'wb') as handle:
-            request = requests.get(self.cover, stream=True)
-            for block in request.iter_content():
-                if not block:
-                    break
-                handle.write(block)
-            handle.close()
                 
     def guardar(self):
         if not os.path.exists(self.directorio):
@@ -173,14 +236,14 @@ class Mix:
         print 'Descargando playlist %s.' % self.nombre    
         for i in range(self.canciones):            
             inicio = time.clock()            
-            cancion = Cancion(*Cancion.atributos(Huron.siguiente(self.id), self.directorio))
+            cancion = Cancion(*Cancion.atributos(EightTracks.siguiente(self.id), self.directorio, self.cover['url']))
             print '\tDescargando canción (%d de %d): %s' % (i + 1, self.canciones, cancion)                          
             cancion.guardar()                                  
             if i > 2:
-                audio = MP3(cancion.archivo)
+                audio = File(cancion.archivo)
                 time.sleep(int(audio.info.length) - int(time.clock() - inicio))
                 
-            Huron.reportar(cancion.id, self.id) 
+            EightTracks.reportar(cancion.id, self.id)             
             
     @staticmethod
     def atributos(r):
@@ -188,32 +251,6 @@ class Mix:
                 r['mix']['cover_urls']['sq500'], 'Descargas/' + r['mix']['name'] + '/')
 
 
-class Huron:
-    
-    headers = {'X-Api-Key': API_KEY, 'X-Api-Version': API_VERSION}
-    urls = {'token':'http://8tracks.com/sets/new.json',
-            'next':'http://8tracks.com/sets/%s/next.json?mix_id=%s',
-            'report':'http://8tracks.com/sets/%s/report.json?track_id=[%s]&mix_id=[%s]'}
-    token = requests.get(urls['token'], headers=headers).json()['play_token']
-
-    def __init__(self, mix_url):                
-        self.mix = Mix(*Mix.atributos(requests.get(mix_url + '.json', headers=Huron.headers).json()))
-
-    def ejecutar(self):
-        self.mix.guardar()
-        print "\n Listo."
-        
-    @staticmethod
-    def siguiente(mix_id):
-        return requests.get(Huron.urls['next'] % (Huron.token, mix_id), headers=Huron.headers).json()
-    
-    @staticmethod
-    def reportar(cancion_id, mix_id):
-        requests.get(Huron.urls['report'] % (Huron.token, cancion_id, mix_id), headers=Huron.headers)    
-
-
 if __name__ == '__main__':
-#     huron = Huron((sys.argv[1] if len(sys.argv) > 1 else raw_input('Ingresa URL del playlist\n')))
-#     huron = Huron('http://8tracks.com/josecols/don-t-close-your-eyes')
-    huron = Huron('http://8tracks.com/rpk213/majestic-casual')    
-    huron.ejecutar()
+    huron = EightTracks((sys.argv[1] if len(sys.argv) > 1 else raw_input('Ingresa URL del playlist\n')))    
+    huron.guardar()
